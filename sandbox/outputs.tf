fb1adc4df1b5a03f63e2dc1d52a660e7f1bc4448
Ben Lackey <ben.lackey@outlook.com>
2018-10-23 12:20:15 -0700

    drop username

--- sandbox/outputs.tf
+++ sandbox/outputs.tf
@@ -1,4 +1,4 @@
-output "1 - Sandbox SSH" { value = "ssh opc@${data.oci_core_vnic.sandbox_vnic.public_ip_address}" }
+output "1 - Sandbox SSH" { value = "ssh ${data.oci_core_vnic.sandbox_vnic.public_ip_address}" }
 output "2 - Cloudera Guided Demo" { value = "http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}/" }
 output "3 - HUE Login" { value = "http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}:8888/" }
 output "4 - Cloudera Manager Login" { value = "http://${data.oci_core_vnic.sandbox_vnic.public_ip_address}:7180/cmf/" }
