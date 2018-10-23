97293000e922b7b5a6c458856810755876ecd9d0
Ben Lackey <ben.lackey@outlook.com>
2018-10-23 12:35:50 -0700

    back to descriptive names

--- sandbox/compute.tf
+++ sandbox/compute.tf
@@ -14,7 +14,7 @@ resource "oci_core_instance" "Sandbox" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("scripts/boot.sh"))}"
+    user_data           = "${base64encode(file("scripts/cloud-init.sh"))}"
   }
 
   timeouts {
