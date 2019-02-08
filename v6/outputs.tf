440437b9ab14f77f31b63a253fa28729ff7289fc
Zachary Smith <Zachary.Smith@oracle.com>
2019-02-08 17:56:19 +0000

    Fixed output syntax

--- v6/outputs.tf
+++ v6/outputs.tf
@@ -5,4 +5,4 @@ output "Hostname List" { value = "${oci_core_instance.Worker.*.hostname_label} $
 output "1 - Bastion SSH Login" { value = "ssh -i ${var.ssh_keypath} opc@${data.oci_core_vnic.bastion_vnic.public_ip_address}" }
 output "2 - Cloudera Manager URL" { value = "http://${data.oci_core_vnic.utility_node_vnic.public_ip_address}:7180/cmf/" }
 output "3 - Cloudera Manager Recent Commands" { value = "http://${data.oci_core_vnic.utility_node_vnic.public_ip_address}:7180/cmf/commands/commands" }
-output "4 - DEPLOYMENT COMMAND" { value = "./scripts/deploy_on_oci.sh -m ${data.oci_core_vnic.utility_node_vnic.public_ip_address} -d ${ceil(((var.hdfs_usable_in_gbs*3)/var.data_blocksize_in_gbs)/var.worker_node_count)} -w ${var.worker_instance_shape}" }
+output "4 - DEPLOYMENT COMMAND" { value = "python scripts/deploy_on_oci.py -m ${data.oci_core_vnic.utility_node_vnic.public_ip_address} -d ${ceil(((var.hdfs_usable_in_gbs*3)/var.data_blocksize_in_gbs)/var.worker_node_count)} -w ${var.worker_instance_shape}" }
