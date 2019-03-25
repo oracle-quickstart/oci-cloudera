fb374ecc6142927711fcef46a1bd10e1ef5cf135
Zachary Smith <Zachary.Smith@oracle.com>
2019-03-25 18:07:24 +0000

    Pathing fix

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -14,7 +14,7 @@ resource "oci_core_instance" "Utility" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("scripts/cm_boot_mysql.sh"))}"
+    user_data           = "${base64encode(file("../scripts/cm_boot_mysql.sh"))}"
   }
 
   timeouts {
@@ -40,7 +40,7 @@ resource "oci_core_instance" "Master" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("scripts/boot.sh"))}"
+    user_data           = "${base64encode(file("../scripts/boot.sh"))}"
   }
 
   timeouts {
@@ -64,7 +64,7 @@ resource "oci_core_instance" "Bastion" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("scripts/boot.sh"))}"
+    user_data           = "${base64encode(file("../scripts/boot.sh"))}"
   }
 
   timeouts {
@@ -90,7 +90,7 @@ resource "oci_core_instance" "Worker" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("scripts/boot.sh"))}"
+    user_data           = "${base64encode(file("../scripts/boot.sh"))}"
   }
 
   timeouts {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,4 +4,4 @@ output "0 - INFO - Block Volume Size (HDFS)" { value = "${var.data_blocksize_in_
 output "1 - Bastion SSH Login" { value = "ssh -i ${var.ssh_keypath} opc@${data.oci_core_vnic.bastion_vnic.public_ip_address}" }
 output "2 - Cloudera Manager URL" { value = "http://${data.oci_core_vnic.utility_node_vnic.public_ip_address}:7180/cmf/" }
 output "3 - Cloudera Manager Recent Commands" { value = "http://${data.oci_core_vnic.utility_node_vnic.public_ip_address}:7180/cmf/commands/commands" }
-output "4 - DEPLOYMENT COMMAND" { value = "python scripts/deploy_on_oci.py -B -m ${data.oci_core_vnic.utility_node_vnic.public_ip_address} -d ${ceil(((var.hdfs_usable_in_gbs*3)/var.data_blocksize_in_gbs)/var.worker_node_count)} -w ${var.worker_instance_shape}" }
+output "4 - DEPLOYMENT COMMAND" { value = "python ../scripts/deploy_on_oci.py -B -m ${data.oci_core_vnic.utility_node_vnic.public_ip_address} -d ${ceil(((var.hdfs_usable_in_gbs*3)/var.data_blocksize_in_gbs)/var.worker_node_count)} -w ${var.worker_instance_shape}" }
