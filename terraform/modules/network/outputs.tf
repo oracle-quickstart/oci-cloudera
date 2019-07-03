aaa18b498a98647a3e4cc068afc05438d777c982
Zachary Smith <Zachary.Smith@oracle.com>
2019-07-03 17:12:17 +0000

    Additional network output fix.

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,13 +1,13 @@
 output "private-id" {
-	value = "${oci_core_subnet.private.id[var.availability_domain - 1]}"
+	value = "${oci_core_subnet.private.id}"
 }
 
 output "public-id" {
-        value = "${oci_core_subnet.public.id[var.availability_domain - 1]}"
+        value = "${oci_core_subnet.public.id}"
 }
 
 output "bastion-id" {
-        value = "${oci_core_subnet.bastion.id[var.availability_domain - 1]}"
+        value = "${oci_core_subnet.bastion.id}"
 }
 
 output "vcn-dn" {
