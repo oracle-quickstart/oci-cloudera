8e93214175c603f15646b5e46d4266084e5f7f13
Zachary Smith <Zachary.Smith@oracle.com>
2019-07-03 16:51:19 +0000

    Removed multi Subnet lookup from Network output

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,13 +1,13 @@
 output "private-id" {
-	value = "${oci_core_subnet.private.*.id[var.availability_domain - 1]}"
+	value = "${oci_core_subnet.private.id[var.availability_domain - 1]}"
 }
 
 output "public-id" {
-        value = "${oci_core_subnet.public.*.id[var.availability_domain - 1]}"
+        value = "${oci_core_subnet.public.id[var.availability_domain - 1]}"
 }
 
 output "bastion-id" {
-        value = "${oci_core_subnet.bastion.*.id[var.availability_domain - 1]}"
+        value = "${oci_core_subnet.bastion.id[var.availability_domain - 1]}"
 }
 
 output "vcn-dn" {
