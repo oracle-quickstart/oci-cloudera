e6a93cf207acf2a94daf4fcb0a05b61596c6e8cf
Zachary Smith <Zachary.Smith@oracle.com>
2020-02-03 21:15:16 +0000

    Bugfix for Block Volumes on DenseIO workers, logic was not working correctly and causing sanity check to fail.

--- modules/worker/variables.tf
+++ modules/worker/variables.tf
@@ -36,30 +36,11 @@ variable "worker_node_count" {
   default = "5"
 }
 
-# Size of each Block Volume used for HDFS /data/
-# Minimum recommended size is 700GB per Volume to achieve max IOPS/MBps
-# Note that total HDFS capacity per worker is limited by this size.  U
-# Here is a total capacity per worker list for reference (using 30 volumes per worker):
-# 700GB Volume Size = 21 TB per worker
-# 1000GB Volume Size = 30 TB per worker
-# 2000GB Volume Size = 60 TB per worker
-
 variable "data_blocksize_in_gbs" {
   default = "700"
 }
 
-# Number of Block Volumes per Worker
-# Minimum recommended is 3 - Scale up to 32 per compute host
-# 5 workers @ 700GB Volume Size = Max HDFS Capacity 105 TB, 35 TB Usable with 3x Replication
-# 10 workers @ 1TB Volume Size = Max HDFS Capacity 300 TB, 100 TB Usable with 3x Replication
-# 10 workers @ 2TB Volume Size = Max HDFS Capacity 600 TB, 200 TB Usable with 3x Replication
-# If using DenseIO local storage only - set this to '0'
-# If using Heterogenous storage, this will add Block Volume capacity on top of Local storage.
-# When using Heterogenous storage - be sure to modify the deploy_on_oci.py and set data_tiering flag to 'True'
-
-variable "block_volumes_per_worker" {
-   default = "3"
-}
+variable "block_volumes_per_worker" {}
 
 variable "vpus_per_gb" {
    default = "10" 
