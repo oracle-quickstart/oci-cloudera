2d556f24a40f9477c534f41c99e94d74d0480e17
Zachary Smith <Zachary.Smith@oracle.com>
2020-03-18 15:39:09 +0000

    Build workflow output enhancement

--- build.sh
+++ build.sh
@@ -2,28 +2,28 @@
 cd $(dirname $0)
 SCRIPT_DIR=$(pwd)
 
-echo "Cleaning build folder"
+echo "-->Cleaning build folder"
 rm -rf $SCRIPT_DIR/build
 mkdir -p $SCRIPT_DIR/build
 
-echo "Creating BYOL stack"
+echo "-->Creating BYOL stack"
 folder=$(mktemp -d "cloudera-XXXXX")
 mkdir -p $folder
 cd $folder
-echo "Copying top level Terraform"
+echo "-->Copying top level Terraform"
 cp $SCRIPT_DIR/*.tf .
 cp $SCRIPT_DIR/*.yaml .
-echo "Copying Modules"
-rsync -apxrv $SCRIPT_DIR/modules/ .
-echo "Copying Scripts"
+echo "-->Copying Modules"
+rsync -apxr $SCRIPT_DIR/modules/ .
+echo "-->Copying Scripts"
 cp -R $SCRIPT_DIR/scripts/ .
 rm -rf .terraform
-echo "Showing contents of build root:"
+echo "-->Showing contents of build root:"
 ls -la
-echo "Creating ZIP archive"
+echo "-->Creating ZIP archive"
 zip -r $SCRIPT_DIR/build/cloudera-byol.zip *
-echo "Cleanup $SCRIPT_DIR"
+echo "-->Cleanup $SCRIPT_DIR"
 cd $SCRIPT_DIR
 rm -rf $folder
-echo "Showing contents of $SCRIPT_DIR/build"
+echo "-->Showing contents of $SCRIPT_DIR/build"
 ls -la $SCRIPT_DIR/build
