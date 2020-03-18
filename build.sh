ff496060e09d7ec95c37fb032473d9a696aa3937
Zachary Smith <Zachary.Smith@oracle.com>
2020-03-18 15:36:20 +0000

    Build artifact output enhanced

--- build.sh
+++ build.sh
@@ -8,16 +8,22 @@ mkdir -p $SCRIPT_DIR/build
 
 echo "Creating BYOL stack"
 folder=$(mktemp -d "cloudera-XXXXX")
-
 mkdir -p $folder
 cd $folder
+echo "Copying top level Terraform"
 cp $SCRIPT_DIR/*.tf .
 cp $SCRIPT_DIR/*.yaml .
-cp -R $SCRIPT_DIR/modules/ .
+echo "Copying Modules"
+rsync -apxrv $SCRIPT_DIR/modules/ .
+echo "Copying Scripts"
 cp -R $SCRIPT_DIR/scripts/ .
 rm -rf .terraform
+echo "Showing contents of build root:"
 ls -la
+echo "Creating ZIP archive"
 zip -r $SCRIPT_DIR/build/cloudera-byol.zip *
+echo "Cleanup $SCRIPT_DIR"
 cd $SCRIPT_DIR
 rm -rf $folder
+echo "Showing contents of $SCRIPT_DIR/build"
 ls -la $SCRIPT_DIR/build
